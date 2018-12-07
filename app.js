753254f6bf1cff40172b107775ea678907cb1841
root <root@DESKTOP-R86V2RV.localdomain>
2018-12-06 18:36:23 -0800

    all working except sort

--- app.js
+++ app.js
@@ -2,38 +2,44 @@ let hornedBeasts = [];
 let moreHornedBeasts = [];
 
 function readJson () {
-  
+
+  $('section').empty()
+  hornedBeasts = []
   $.get('page-1.json', 'json')
     .then(data => {
+      console.log('HITTING readJson function***')
       data.forEach(generic =>{
         new HornMaker(generic, 1);
       });
       hornedBeasts.forEach( (horn) => {
         $('#photo-template').append(horn.toHtml());
         horn.menu();
-        ;
-      } );  
-      
-    })
-    
+        
+      });
+
+    });
+
 }
 
 function readMoreJson () {
-  console.log('reading more json'); 
+  $('section').empty()
+  moreHornedBeasts = [];
+  console.log('reading more json');
   $.get('page-2.json', 'json')
     .then(data => {
       data.forEach(moreData => {
         new HornMaker(moreData, 2);
       });
+      console.log('MORE HORNED BEATSTSKF', moreHornedBeasts)
       moreHornedBeasts.forEach( (horn) => {
         $('#photo-template').append(horn.toHtml());
-        console.log('trying again');
+        // horn.menu();
+
       } );
     });
 }
 
 $(() => readJson());
-// $(() => readMoreJson());
 
 
 let HornMaker = function(catchall, page){
@@ -43,8 +49,8 @@ let HornMaker = function(catchall, page){
   this.horns = catchall.horns;
   this.keyword = catchall.keyword;
   this.page = page;
-  
-  
+
+
   if(page === 1) {
     this.class= 'one';
     hornedBeasts.push(this);
@@ -56,7 +62,7 @@ let HornMaker = function(catchall, page){
 };
 
 HornMaker.prototype.menu = function () {
-  
+
   $('select').append('<option class = "option"></option>');
   let $option = $('option[class="option"]');
   $option.attr('value', this.keyword);
@@ -70,42 +76,40 @@ HornMaker.prototype.menu = function () {
 //Hornmaker  prototype//
 HornMaker.prototype.toHtml = function() {
   let templateHtml = $('#horn-template').html();
-  
+
   // 2. Use Handlebars to "compile" the HTML
   let compileHornTemplate = Handlebars.compile(templateHtml);
   // 3. Do not forget to return the HTML from this method
   let newHornTemplate = compileHornTemplate(this);
-  
+
   return newHornTemplate;
 };
 
-// function renderMoreBeasts() {
-//   let mainy = $('div[name="hide"]').hide();
-//   moreHornedBeasts.forEach(beast => {
-//     beast.render();
-//   });
-// }
 
-// function renderOriginalBeasts() {
-//   let mainy = $('div[name="hide"]').hide();
-//   hornedBeasts.forEach(beast => {
-//     beast.render();
-//   });
-// }
 //button for additional horned beasts
 $('button[name=placeholder]').on('click', function(){
-
-readMoreJson();
+  console.log('HITTING THIS BUTTON')
+  readMoreJson();
 }
 );
 //button to go back to original images
-$('button[name=original_images]').on('click', readMoreJson);
+$('button[name=original_images]').on('click', ()=>{
+  console.log('HITTING ORIGINAL PAGE')
+  readJson()
+});
+
+
 
 // select box filtering
 
-$('select[name="horned"]').on('change', function() {
+$('select').on('change', function() {
   let $selection = $(this).val();
-  $('div').hide();
+  
+  console.log('DIVS TO HIDE',  $('section div').hide() )
+  $('section div').hide();
+
+  console.log('OPTION SELECTED', $selection)
+  console.log($('section div'))
   $(`div[class="${$selection}"]`).show();
 });
 
